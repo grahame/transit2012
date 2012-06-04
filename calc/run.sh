9b6161ceda343b61dafb526fc0e0f598e9b8baff
Grahame Bowland <grahame@angrygoats.net>
2012-06-04 04:59:27 -0400

    add my name

--- calc/run.sh
+++ calc/run.sh
@@ -3,8 +3,10 @@
 cd ~/code/transit2012/calc/ || exit 1
 
 python3 transittweets.py venusobs 'transit04' > 2004.csv && 
-python3 calc.py 2004 2004.csv > ../site/calc/2004.json
+python3 calc.py 2004 2004.csv > ../site/calc/2004.json.n && 
+mv ../site/calc/2004.json.n ../site/calc/2004.json
 
 python3 transittweets.py venusobs venusobs > transit12.csv && 
-python3 calc.py 2012 transit12.csv > ../site/calc/2012.json
+python3 calc.py 2012 transit12.csv > ../site/calc/2012.json.n &&
+mv ../site/calc/2012.json.n ../site/calc/2012.json
 
